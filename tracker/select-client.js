88f13c7f08ea41c6309803806bac40a3227d092d
Eric Silva <ericsilvap13@gmail.com>
2025-10-16 22:08:35 -0300

    add select-client style; add base select buttons

--- tracker/select-client.js
+++ tracker/select-client.js
@@ -1,3 +1,5 @@
+const websocket = new WebSocket('ws://localhost:5679/')
+
 function createClient() {
   const clientElement = document.createElement('div')
   const clientLink = document.createElement('a')
@@ -10,7 +12,19 @@ function createClient() {
   return clientElement
 }
 
+websocket.addEventListener('open', () => {
+  websocket.send(JSON.stringify({ type: 'tracker', action: 'get_senders' }))
+})
+
+// websocket.onmessage = ({ data }) => {
+//   const data = JSON.parse(data)
+//   alert(data.senders)
+// }
+
 document.addEventListener('DOMContentLoaded', () => {
   const clientsContainer = document.querySelector('.clients-container')
+
+  clientsContainer.appendChild(createClient())
+  clientsContainer.appendChild(createClient())
   clientsContainer.appendChild(createClient())
 })
