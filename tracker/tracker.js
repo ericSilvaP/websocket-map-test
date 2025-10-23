7e9954395cd5a545c1f57f528057afbe8cd6f1a7
Eric Silva <ericsilvap13@gmail.com>
2025-10-23 10:40:51 -0300

    add distance between markers

--- tracker/tracker.js
+++ tracker/tracker.js
@@ -3,9 +3,32 @@ let map = L.map('map').setView([51.505, -0.09], 13)
 let userMarker = L.marker([51.5, -0.09]).addTo(map)
 let productMarker = L.marker([51.5, -0.09]).addTo(map)
 let userCoords = { lat: '', lng: '' }
+let productCoords = { lat: '', lng: '' }
 const params = new URLSearchParams(window.location.search)
 const senderId = params.get('id')
 const coordContainer = document.querySelector('.ex')
+
+function calcDistance(userPos, productPos) {
+  const R = 6372.795477598 // raio médio quadrático da Terra em km
+
+  // Converter graus para radianos
+  const toRad = (deg) => (deg * Math.PI) / 180
+
+  const lat1 = toRad(userPos.lat)
+  const lon1 = toRad(userPos.lng)
+  const lat2 = toRad(productPos.lat)
+  const lon2 = toRad(productPos.lng)
+
+  // Fórmula da distância (lei dos cossenos esférica)
+  const distance =
+    R *
+    Math.acos(
+      Math.sin(lat1) * Math.sin(lat2) +
+        Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2)
+    )
+
+  return distance
+}
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -40,10 +63,21 @@ websocket.addEventListener('open', () => {
 // Atualiza marcador ao receber coordenadas
 websocket.onmessage = ({ data }) => {
   const pos = JSON.parse(data)
-  const lat = pos.lat
-  const lng = pos.lng
+  productCoords.lat = pos.lat
+  productCoords.lng = pos.lng
 
-  productMarker.setLatLng([lat, lng])
+  updateDistance()
+  productMarker.setLatLng([productCoords.lat, productCoords.lng])
   map.setView([lat, lng], 13)
   coordContainer.textContent = `${pos.lat}, ${pos.lng}`
+}
+
+function updateDistance() {
+  if (!userCoords.lat && !userCoords.lng) {
+    setTimeout(updateDistance, 2000)
+  }
+  const distance = calcDistance(userCoords, productCoords)
+  document.querySelector('.distance').textContent = `Distância: ${Number(
+    distance.toFixed(2)
+  ).toLocaleString('pt-BR')} km`
 }
